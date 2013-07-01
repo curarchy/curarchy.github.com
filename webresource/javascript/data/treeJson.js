160ad73c9a5edd2d4ccf85f5a93e10f643b21b27
curarchy <curarpiktchen@hotmail.com>
2013-07-01 15:11:17 +0800

    modify

---
+++ webresource/javascript/data/treeJson.js
@@ -0,0 +1,189 @@
+﻿var treeJson = [{
+    id: 0,
+    title: "华北",
+    key: "hb",
+    children: [{
+        id: 1,
+        title: "北京",
+        key: "beijing"
+    }, {
+        id: 2,
+        title: "内蒙",
+        key: "neimeng"
+    }, {
+        id: 3,
+        title: "山西",
+        key: "shanxi"
+    }, {
+        id: 4,
+        title: "河北",
+        key: "hebei"
+    }
+		]
+}, {
+    id: 10,
+    title: "东北",
+    key: "db",
+    children: [{
+        id: 11,
+        title: "黑龙江",
+        key: "heilongjiang"
+    }, {
+        id: 12,
+        title: "辽宁",
+        key: "liaoning"
+    }, {
+        id: 13,
+        title: "吉林",
+        key: "jilin",
+        checked: true
+    }
+		]
+}, {
+    id: 20,
+    title: "华东",
+    key: "hd",
+    children: [{
+        id: 21,
+        title: "上海",
+        key: "shanghai",
+        children: [{
+            id: 211,
+            title: "浦西",
+            key: "puxi",
+            children: [{
+                id: 2110,
+                title: "黄浦",
+                key: "huangpu",
+                children: [{
+                    id: 21100,
+                    title: "新天地",
+                    key: "xintiandi"
+                }, {
+                    id: 21101,
+                    title: "人民广场",
+                    key: "renminguangchang",
+                    checked: true
+                }
+								]
+            }, {
+                id: 2111,
+                title: "徐汇",
+                key: "xuhui"
+            }
+						]
+        }, {
+            id: 210,
+            title: "浦东",
+            key: "pudong",
+            children: [{
+                id: 2101,
+                title: "三林",
+                key: "sanlin",
+                children: [{
+                    id: 21010,
+                    title: "永泰",
+                    key: "yongtai"
+                }]
+            }, {
+                id: 2102,
+                title: "北蔡",
+                key: "beicai"
+            }, {
+                id: 2103,
+                title: "金桥",
+                key: "jinqiao",
+                checked: true,
+                children: [{
+                    id: 21031,
+                    title: "temp",
+                    key: "temp"
+                }]
+            }
+						]
+        }
+				]
+    }, {
+        id: 22,
+        title: "福建",
+        key: "fujian"
+    }, {
+        id: 23,
+        title: "浙江",
+        key: "zhejiang"
+    }, {
+        id: 24,
+        title: "江苏",
+        key: "jiangsu"
+    }, {
+        id: 25,
+        title: "安徽",
+        key: "anhui"
+    }, {
+        id: 26,
+        title: "山东",
+        key: "shandong"
+    }, {
+        id: 27,
+        title: "江西",
+        key: "jiangxi"
+    }
+		]
+}, {
+    id: 30,
+    title: "华中华南",
+    key: "hzhn",
+    children: [{
+        id: 31,
+        title: "广东",
+        key: "shanghai"
+    }, {
+        id: 32,
+        title: "广西",
+        key: "fujian"
+    }, {
+        id: 33,
+        title: "湖南",
+        key: "zhejiang"
+    }, {
+        id: 34,
+        title: "湖北",
+        key: "jiangsu"
+    }, {
+        id: 35,
+        title: "海南",
+        key: "anhui"
+    }, {
+        id: 36,
+        title: "河南",
+        key: "shandong"
+    }
+		]
+}, {
+    id: 40,
+    title: "西南",
+    key: "xn",
+    children: [{
+        id: 41,
+        title: "西藏",
+        key: "xizang"
+    }, {
+        id: 42,
+        title: "云南",
+        key: "yunnan"
+    }, {
+        id: 43,
+        title: "四川",
+        key: "sichuan"
+    }, {
+        id: 44,
+        title: "贵州",
+        key: "guizhou"
+    }
+		]
+}, {
+    id: 50,
+    title: "新疆丝路",
+    key: "xjsl"
+}
+];
