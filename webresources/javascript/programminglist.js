e1ec4feea269bb202ef384a0f74f08a967a24a44
curarchy <curarpiktchen@hotmail.com>
2013-07-09 11:12:00 +0800

    modify

--- webresources/javascript/programminglist.js
+++ webresources/javascript/programminglist.js
@@ -1,3 +1,18 @@
-﻿(function(){
-	
+﻿(function() {
+	window._$ = window._$ || {};
+
+	var listTemplate = "<pre>" +
+		"<p>{{title}}</p>" +
+		"<p>{{summary}}</p>" +
+		"</pre>";
+
+	_$.programminglist = {
+		init: function() {
+			var data = _$.programmingres.getList();
+			$.each(data, function(index, item) {
+				var str = _$.stringFormat(listTemplate, item);
+				$("#listarea").append($(str));
+			});
+		}
+	};
 })();
