56098b76843a08dec1edef3d591fb7bc54ebf71f
curarchy <curarpiktchen@hotmail.com>
2013-07-10 10:47:44 +0800

    modify

--- webresources/javascript/programminglist.js
+++ webresources/javascript/programminglist.js
@@ -2,16 +2,25 @@
 	window._$ = window._$ || {};
 
 	var listTemplate = "<pre>" +
-		"<p>{{title}}</p>" +
+		"<h4 class='listtitle'><a>{{title}}</a></h4>" +
 		"<p>{{summary}}</p>" +
+		"<p class='listbottom clearfix'></p>"+
 		"</pre>";
+
+	var tagTemplate = "<span style='float:right;margin:0 4px;' class='label label-info'>{0}</span>";
 
 	_$.programminglist = {
 		init: function() {
 			var data = _$.programmingres.getList();
 			$.each(data, function(index, item) {
 				var str = _$.stringFormat(listTemplate, item);
-				$("#listarea").append($(str));
+				var _str = $(str);
+				if (item.tag && item.tag.length) {
+					$.each(item.tag, function(index1, item1) {
+						_str.find(".listbottom").append($(_$.stringFormat(tagTemplate, item1)));
+					});
+				}
+				$("#listarea").append(_str);
 			});
 		}
 	};
--- webresources/javascript/programmingres.js
+++ webresources/javascript/programmingres.js
@@ -1,15 +1,21 @@
 ﻿(function() {
 	var programmingcontent = [{
-			title: "自动完成",
-			summary: "自动完成 summary",
-			key: "201307081",
-			tag: ["jquery", "javascript"],
+			title: "这是测试的第三篇文章",
+			summary: "主要测试List/Block切换",
+			key: "201307091",
+			tag: ["test", "website"],
 			img: ""
 		}, {
-			title: "test",
-			summary: "summary test",
+			title: "这是测试的第二篇文章,title比较长，不知道会发生啥。。。。。。。。。。。。。。。。",
+			summary: "主要测试超长的title和summary，所以这里会长一点长一点长一点长一点长一点长一点长一点长一点长一点",
+			key: "201307081",
+			tag: ["test", "website"],
+			img: ""
+		}, {
+			title: "这是测试的第一篇文章",
+			summary: "主要测试跳转是否正常，列表是否显示正常。",
 			key: "201307082",
-			tag: ["jquery", "html"],
+			tag: ["test", "website"],
 			img: ""
 		}
 	];
